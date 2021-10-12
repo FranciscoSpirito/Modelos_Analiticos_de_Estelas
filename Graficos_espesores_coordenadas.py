f0e59e5b015ba81cc9e94ad95594e33c19000d73
Francisco Spirito <fspirito@fi.uba.ar>
2021-10-12 19:55:31 -0300

    Se cambiaron las funciones por los metodos de turbina, para que cuando revisemos algo revisemos los metodos de turbina y no haya q andar copiando y pegando

--- Graficos_espesores_coordenadas.py
+++ Graficos_espesores_coordenadas.py
@@ -1,12 +1,13 @@
 import matplotlib.pyplot as plt
 import numpy as np
-from definicion_de_espesor import definicion_de_espesor
-from coordenadas_y_areas_normalizadas import coordenadas_y_areas_normalizadas
+from Turbina import Turbina
+from Coord import Coord
 
+turbina_0 = Turbina(90, Coord(np.array([(0),(0),250])))
 lista_n_espesores = np.arange(1,1000,10)
 lista_e = []
 for n in lista_n_espesores:
-    lista_e.append(definicion_de_espesor(n))
+    lista_e.append(turbina_0.definicion_de_espesor(n))
 plt.plot(lista_n_espesores,lista_e, 'o')
 plt.title('Espesor Normalizado')
 plt.xlabel('N')
@@ -14,10 +15,10 @@ plt.ylabel('e')
 plt.show()
 
 
-lista_n_coord = [1, 5, 10, 50, 100, 200, 400, 1000]
+lista_n_coord = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30]
 for n in lista_n_coord:
-    e = definicion_de_espesor(n)
-    lista_coord, lista_dAi = coordenadas_y_areas_normalizadas(n,e)
+    e = turbina_0.definicion_de_espesor(n)
+    lista_coord, lista_dAi = turbina_0.coordenadas_y_areas_normalizadas(n,e)
     lista_coord_y = [coordenada.y for coordenada in lista_coord]
     lista_coord_z = [coordenada.z for coordenada in lista_coord]
     plt.plot(lista_coord_y, lista_coord_z, 'o', linewidth=3)
