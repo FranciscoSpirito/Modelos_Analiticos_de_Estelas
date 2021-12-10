52d7e52b8fc0efd0f85fbf9eba5b8160b4891419
Francisco Spirito <fspirito@fi.uba.ar>
2021-12-09 21:53:28 -0300

    Se realiza una nueva funcion calcular_potencia... para obviar el problema de la coordenada y se modifica el prueba_para_debugg.. para usar esta funcion

---
+++ calcular_potencia_del_parque_con_terreno.py
@@ -0,0 +1,89 @@
+from __future__ import division
+import numpy as np
+from scipy.interpolate import interp1d
+from Modelo import Modelo
+from Parque_de_turbinas import Parque_de_turbinas
+from Turbina import Turbina
+from Coord import Coord
+from Estela import Estela
+from Iso_Superficie import Iso_Superficie
+
+
+# calcular_u_en_coord:
+# .INPUT
+# modelo_deficit: instancia de Modelo, define la forma con la que se ajustara el perfil de la estela
+# metodo_superposicion: forma en la que se superone la estela (lineal, dominante, etc)
+# coord: instancia de Coord, es el punto del espacio en el que se busca calcular el viento u
+# parque_de_turbinas: instancia de Parque_de_turbinas, incluye toda la informacion relevante al
+# parque que perturba el flujo
+# iso_s: intancia de Iso_superficie, contiene la informacion sobre el flujo base
+# lista_coord_normalizadas: coordenadas de un disco de radio 1 ubicado en el (0,0,0)
+# lista_dAi_normalizadas: areas de los diferenciales de un disco de radio 1 ubicado en el (0,0,0)
+# u_inf: instancia de U_inf, contiene la informacion del perfil de velocidades (log o cte)
+# u: viento en la coordenada coord al atravezar el parque con las condiciones de entrada definidas
+
+
+def calcular_potencia_del_parque_con_terreno(modelo_deficit, metodo_superposicion, parque_de_turbinas, u_inf, iso_s,
+                                                lista_coord_normalizadas, lista_dAi_normalizados):
+
+    parque_de_turbinas.ordenar_turbinas_de_izquierda_a_derecha()
+    lista_de_turbinas = parque_de_turbinas.turbinas
+    # lista que guardara los deficits normalizados generados por todas las turbinas a la izquierda de coord
+    deficit_normalizado_en_coord = []
+    # Radio maximo de la "zona influyente" de interseccion con las ldc de las turbinas aguas abajo
+    # (turbinas con estelas fuera de esta zona, no se tomara en cuenta su influencia)
+    rmax = 500
+    # Puntos dentro de la zona influyente (normalizados)
+    nsamp = 21
+    samp_l = np.linspace(-1, 1, nsamp)
+
+    # Loop para calculo de deficits en la coordenada generado por las turbinas aguas abajo
+    for turbina_selec in lista_de_turbinas:
+
+        turbina_selec.desnormalizar_coord_y_areas(lista_coord_normalizadas, lista_dAi_normalizados)
+        turbinas_a_la_izquierda_de_turbina_selec = parque_de_turbinas.turbinas_a_la_izquierda_de_una_coord(turbina_selec.coord)
+        cantidad_turbinas_izquierda_de_selec = len(turbinas_a_la_izquierda_de_turbina_selec)
+
+        #  Calculo de zona influyente de la turbina seleccionada samp_xy
+        coord_xy = (turbina_selec.coord.x, turbina_selec.coord.y)
+        u0, v0 = turbina_selec.U_f_base[0], turbina_selec.U_f_base[1]
+        muv = np.sqrt(u0 ** 2 + v0 ** 2)
+        samp_xy = (np.vstack((samp_l * rmax * -v0 / muv, samp_l * rmax * u0 / muv)).T + coord_xy).T
+
+        # lista que guardara los deficits generados sobre las coordenadas dentro del disco de turbina_selec
+        arreglo_deficit = []
+
+        # separa el caso en el que la turbina es la 'mas a la izquierda', es decir, es la turbina que recibe
+        # el viento limpio, sin verse afectado por otra turbina
+        if cantidad_turbinas_izquierda_de_selec == 0:
+            turbina_virtual = Turbina(turbina_selec.d_0, Coord(
+                np.array([turbina_selec.coord.x, turbina_selec.coord.y, turbina_selec.coord.z])))
+            turbina_virtual.c_T = 0
+            turbina_virtual.t = turbina_selec.t
+            turbinas_a_la_izquierda_de_turbina_selec = [turbina_virtual]
+
+        # Loop para calculo de deficits en la turbina_selec generado por las turbinas aguas abajo
+        for turbina_a_la_izquierda in turbinas_a_la_izquierda_de_turbina_selec:
+
+            # calculo de interseccion con zona influyente de turbina_selec, si existe interseccion samp_l_int sera un valor normalizado entre -1 y 1
+            # en caso contrario sera None
+            valores_x = [ iso_s._interp_t(samp_xy[0, i], samp_xy[1 , i]).item() for i in range(len(samp_xy[0]))]
+            samp_l_int = interp1d(valores_x, samp_l, kind='cubic', bounds_error=False)(turbina_a_la_izquierda.t)
+
+            # calculo del deficit en las coord de la turbina_selec
+            for coordenada in turbina_selec.lista_coord:
+                deficit_normalizado_en_coordenada = modelo_deficit.evaluar_deficit_normalizado(
+                    turbina_a_la_izquierda, coordenada, iso_s, samp_l_int, rmax, v0, u0, muv, coord_xy)
+                arreglo_deficit.append(deficit_normalizado_en_coordenada)
+
+
+        # crea una instancia de Estela con los datos calculados sobre las coordenadas aleatorias
+        estela_sobre_turbina_selec = Estela(arreglo_deficit, len(turbina_selec.lista_coord), cantidad_turbinas_izquierda_de_selec)
+        estela_sobre_turbina_selec.merge(metodo_superposicion)
+
+        # Se calculan C_T C_P y Potencia de cada turbina
+        turbina_selec.u_adentro_disco_con_terreno(u_inf, estela_sobre_turbina_selec, parque_de_turbinas.z_mast, parque_de_turbinas.z_0)
+        turbina_selec.calcular_c_T_Int_Det()
+        turbina_selec.calcular_c_P_Int_Det()
+        turbina_selec.calcular_P_Int_Det()
+
--- prueba_para_debugg_con terreno.py
+++ prueba_para_debugg_con terreno.py
@@ -8,10 +8,11 @@ from U_inf import U_inf
 from Gaussiana_adaptado_al_Terreno import Gaussiana_adaptado_al_Terreno
 from Iso_Superficie import Iso_Superficie
 from calcular_u_en_coord_con_terreno import calcular_u_con_terreno
+from calcular_potencia_del_parque_con_terreno import calcular_potencia_del_parque_con_terreno
 from load_txt_datos import cargar_datos
 
 # Carga datos desdes .raw o .txt e inicializa la clase Iso_Superficie
-ruta  = r"C:\Users\chesp\Documents\Ingenieria Mecanica\Tesis\Modelos_Analiticos_de_Estelas\Datos_Rawson_sin_Turbinas\U_superficie_gondolas.Dir.45.00.U8.50.raw"
+ruta  = r"C:\Users\chesp\Documents\Ingenieria Mecanica\Tesis\Modelos_Analiticos_de_Estelas\Datos_Rawson_sin_Turbinas\U_superficie_gondolas.Dir.180.00.U8.50.raw"
 xl, yl, zl, ul, vl, wl = cargar_datos('isosuperficie', ruta)
 x, y, z, u, v, w = np.array(xl), np.array(yl), np.array(zl), np.array(ul), np.array(vl), np.array(wl)
 iso_s = Iso_Superficie(x, y, z, u, v, w)
@@ -26,22 +27,15 @@ u_inf.perfil = 'log'
 ruta = r"C:\Users\chesp\Documents\Ingenieria Mecanica\Tesis\Modelos_Analiticos_de_Estelas\Coordenadas_de_turbinas\Coordenadas_turbinas_parque_Rawson.txt"
 turbinas_list  = cargar_datos('coordenadas_turbinas', ruta)
 
-# turbina_0 = Turbina_Rawson(Coord(np.array([(2500),(2500),260])))
-# turbina_1 = Turbina_Rawson(Coord(np.array([(2000),(2000),260])))
-# turbina_2 = Turbina_Rawson(Coord(np.array([2300,(2300),260])))
-# turbina_3 = Turbina_Rawson(Coord(np.array([(2800),(2800),260])))
-# turbina_4 = Turbina_Rawson(Coord(np.array([(3000),(3000),260])))
-# turbinas_list = [turbina_0, turbina_1, turbina_2, turbina_3, turbina_4]
-
-fig1 , axes1 = plt.subplots(2,1)
+fig1 , axes1 = plt.subplots(1,1)
 # Ploteo posicion sin rotar de las turbinas
 turbinas_x = []
 turbinas_y = []
 for turbina in turbinas_list:
     turbinas_x.append(turbina.coord.x)
     turbinas_y.append(turbina.coord.y)
-axes1[0].plot(turbinas_x, turbinas_y, 'o')
-axes1[0].set_title('Posicion de turbinas sin rotar')
+axes1.plot(turbinas_x, turbinas_y, 'o')
+axes1.set_title('Posicion de turbinas sin rotar')
 
 turbina_0 = turbinas_list [0]
 z_mast = turbina_0.coord.z
@@ -57,21 +51,18 @@ lista_coord_normalizadas, lista_dAi_normalizados = turbina_0.coordenadas_y_areas
 
 # Genera malla mas comoda y menos densa
 iso_s.new_grid(100)
-angulo = 45 # angulo de direccion del viento
+angulo = 180 # angulo de direccion del viento
 nstream = 20 # cantidad de streamlines
 # Define la semilla inicial, debe cubrir todo el terreno
 x0, y0 = iso_s.gen_semillas(angulo, nstream)
 dr = 250
 # Calcula las streamlines
 streamlines = [iso_s._makeStreamline(*xy, dr) for xy in zip(x0, y0)]
-# # Grafica streamlines sin rotar
-# for line in streamlines:
-#     axes1[0].plot(line[0], line[1], '.r')
+# Grafica streamlines sin rotar
+for line in streamlines:
+    axes1.plot(line[0], line[1], '.r')
 
-
-
-
-fig2 ,axes2 = plt.subplots(2,1)
+fig2 ,axes2 = plt.subplots(1,1)
 # Rota cooordenadas de Turbinas (las referencia al nuevo sistema de coordenadas alineado con el viento)
 parque_de_turbinas.rotar(angulo)
 # Ploteo posicion de las turbinas rotadas
@@ -80,32 +71,25 @@ turbinas_y = []
 for turbina in turbinas_list:
     turbinas_x.append(turbina.coord.x)
     turbinas_y.append(turbina.coord.y)
-axes2[0].plot(turbinas_x, turbinas_y, 'o')
-axes2[0].set_title('Posicion de turbinas rotadas')
-
+axes2.plot(turbinas_x, turbinas_y, 'o')
+axes2.set_title('Posicion de turbinas rotadas')
 
 # Rota streamlines (las referencia al nuevo sistema de coordenadas alineado con el viento)
 streamlines = iso_s.rotar(angulo, streamlines)
-# # Grafica streamlines rotadas
-# for line in streamlines:
-#     axes2[0].plot(line[0], line[1], '.r')
-
+# Grafica streamlines rotadas
+for line in streamlines:
+    axes2.plot(line[0], line[1], '.r')
+line = streamlines[6]
+axes2.plot(line[0][0], line[1][0], '.y')
+axes2.plot(line[0][len(line[1])-1], line[1][len(line[1])-1], '.k')
 plt.show()
 
-
-# Define los interpoladores de s, t y z
+# Define los interpoladores
 iso_s.redef_interpoladores(streamlines)
 iso_s.flujo_base_turbinas(turbinas_list)
 
-# Coord donde se calcula la velocidad, si se quiere calcular la potencia de todo el parque este debe estar a. arriba de todas las turbinas
-x_o = 5000
-y_o = 5000
-z_o = 260
-coord = Coord(np.array([x_o, y_o, z_o]))
-coord.rotar(angulo)
-
-# Calcula la velocidad, y en el proceso CT, CP, P de las turbinas
-data_prueba = calcular_u_con_terreno(gaussiana_adaptado_al_terreno, 'linear', coord, parque_de_turbinas, u_inf, iso_s, lista_coord_normalizadas,lista_dAi_normalizados)
+# Calcula CT, CP, P de las turbinas
+data_prueba = calcular_potencia_del_parque_con_terreno(gaussiana_adaptado_al_terreno, 'linear', parque_de_turbinas, u_inf, iso_s, lista_coord_normalizadas,lista_dAi_normalizados)
 
 # potencia nominal cuando la turbina trabaja con un viento de 8.2 m/s
 potencia_mast = 949.027296358
